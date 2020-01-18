429fd4f1b420878cb12211ab89458f237e3066c0
Lechrond <chinnot@qq.com>
2020-01-18 18:11:55 +0800

    初始化美团项目

---
+++ 04meituan/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <mt-home></mt-home>
+  </div>
+</template>
+
+<script>
+  import Home from "./components/Home.vue"
+  export default {
+    name: 'app',
+    components: {
+      [Home.name]: Home
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "styles/init.css";
+</style>
---
+++ 04meituan/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home-container">
+    这是首页
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: "mt-home",
+    data() {
+      return {}
+    },
+    components: {}
+  }
+</script>
+
+<style scoped lang='scss'>
+
+</style>
